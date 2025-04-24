dcb2ef0730e04436507bc66d04fef8b19c6f70cb
Ashwin <ashwin.2201098cs@iiitbh.ac.in>
2025-04-24 19:27:39 +0530

    feat: implement user signup and login functionality with validation

--- src/controllers/signup.controller.ts
+++ src/controllers/signup.controller.ts
@@ -1,1 +1,48 @@
 import { asyncHandler,Response,loginFunction } from "../utils";
+import bcrypt from "bcrypt";
+import { signUpUsers } from "../db";
+import {z as zod} from "zod";
+import dotenv from "dotenv";
+import {v4 as uuidv4} from "uuid";
+
+dotenv.config();
+
+const signUpSchema = zod.object({
+    name: zod.string().min(3),
+    email: zod.string().email(),
+    phone: zod.string().min(10).max(10),
+    password: zod.string().min(8),
+})
+
+const signUpFunction = asyncHandler(async (req, res) => {
+    const parsedData = signUpSchema.safeParse(req.body);
+    if (!parsedData.success) {
+        return res.status(400).json(new Response(400, "Validation failed", parsedData.error.errors));
+    }
+    const { name, email, password,phone } = parsedData.data;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const existingUser = signUpUsers.find(user => user.email === email);
+    if (existingUser) {
+        return res.status(400).json(new Response(400, "User already exists", null));
+    }
+    const newUser = {
+        id: uuidv4(),
+        name,
+        email,
+        phone,
+        password: hashedPassword,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+    }
+
+    signUpUsers.push(newUser);
+    const cookieOptions = {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production', 
+    };
+    const token = await loginFunction({ email, password });
+    return res.cookie('token', token, cookieOptions).status(201).json(new Response(201, "User created successfully", { user: newUser, token }));
+
+
+})
--- src/utils/login.ts
+++ src/utils/login.ts
@@ -3,7 +3,7 @@ import {z as zod} from 'zod'
 
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
-
+import {signUpUsers} from '../db'
 import dotenv from 'dotenv'
 
 
@@ -24,7 +24,19 @@ export const loginFunction = async({ email,password } : Login) =>{
         const errors = parsedData.error.errors.map(error => error.message)
         throw new Error(`Validation failed: ${errors.join(', ')}`)
     }
-    
-   
-    
+
+    const { email: parsedEmail, password: parsedPassword } = parsedData.data
+    const user = signUpUsers.find(user => user.email === parsedEmail)
+    if (!user) {
+        throw new Error('User not found')
+    }
+    const isPasswordValid = await bcrypt.compare(parsedPassword, user.password)
+    if (!isPasswordValid) {
+        throw new Error('Invalid password')
+    }
+    const token = jwt.sign({ id: user.id,name:user.name,email:user.email }, process.env.JWT_SECRET as string, {
+        expiresIn: '1h',
+    })
+
+    return token;
 }
